eeea4ba7e79f633d2b42e3dcc33460d66722c664
Yuliia <verbikyullya@gmail.com>
2023-10-30 11:52:19 +0200

    add Employee, Task, Position, TaskType models and set AUTH_USER_MODEL in settings.py = 'catalog.Employee'

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(AbstractUser):
+    position = models.ForeignKey(Position,
+                                 related_name="employees",
+                                 null=True,
+                                 on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "employee"
+        verbose_name_plural = "employees"
+
+    def __str__(self):
+        return (f"{self.username}: "
+                f"{self.first_name} {self.last_name} "
+                f"({self.position})")
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("urgent", "🔴 Urgent!!! 🔴"),
+        ("high", "🟠 High priority! 🟠"),
+        ("medium", "🟡 Medium priority. 🟡"),
+        ("low", "🟢 Low priority. 🟢")
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completer = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=50,
+        choices=PRIORITY_CHOICES
+    )
+    task_type = models.ForeignKey(TaskType,
+                                  related_name="tasks",
+                                  on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Employee,
+                                       related_name="tasks")
+
+    def __str__(self):
+        return self.name
