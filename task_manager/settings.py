1df3ef925e989306331870e94c6a0d9eb130abbc
Yuliia <verbikyullya@gmail.com>
2023-10-31 19:11:50 +0200

    feat: - add base.html, navbar.html, pagination.html - add query_transform.py for better pagination - add styles.css

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
+    "crispy_forms",
     "catalog",
 ]
 
@@ -126,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
