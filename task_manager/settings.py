28fec00440053eba26e9138718091cd19408fb70
Yuliia <verbikyullya@gmail.com>
2023-11-07 10:51:14 +0200

    chore: - upgrade settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -148,3 +148,5 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
